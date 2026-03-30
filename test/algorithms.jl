ed54361169e8b9f975a1fdb303b63ca7aefe7113
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-03-30 15:41:32 +0200

    Tests: check error bounds for each algorithm

--- test/algorithms.jl
+++ test/algorithms.jl
@@ -1,4 +1,5 @@
 # Test the accuracy of twofloat's main algorithms
+# Relative error bounds: cf. paper from J.M. Muller et al.
 # Function inputs/outputs dataset is created by /test/algorithms.ts
 
 using Test
@@ -25,6 +26,9 @@ testset = JSON.parse(json, TestSet; null=NaN)
 
 args_list = testset.argsList
 fn_output = testset.fnOutput
+
+const u = big(2.0)^-precision(Float64)
+const e = big(eps(0.0))
 
 @testset "Error-free transforms" begin
 
@@ -65,6 +69,92 @@ fn_output = testset.fnOutput
         for (i, (x, y)) in enumerate(args)
             zhi, zlo = output[i]
             big(x) * big(y) == big(zhi) + big(zlo)
+        end
+    end
+end
+
+@testset "op21(TwoF64, F64)" begin
+    args = args_list.s21
+
+    @testset "add21 error bound" begin
+        output = fn_output["add21"]
+        @test length(args) == length(output)
+        rel_err = 2u^2
+        abs_err = r -> max(abs(rel_err * r), e)
+        for (i, ((xhi, xlo), y)) in enumerate(args)
+            zhi, zlo = output[i]
+            z = big(zhi) + big(zlo)
+            r = big(xhi) + big(y) + big(xlo)
+            @test abs(z - r) < abs_err(r)
+        end
+    end
+
+    @testset "mul21 error bound" begin
+        output = fn_output["mul21"]
+        @test length(args) == length(output)
+        rel_err = 3u^2/2 + 4u^3
+        abs_err = r -> max(abs(rel_err * r), 2e)
+        for (i, ((xhi, xlo), y)) in enumerate(args)
+            zhi, zlo = output[i]
+            z = big(zhi) + big(zlo)
+            r = big(xhi)*big(y) + big(xlo)*big(y)
+            @test abs(z - r) < abs_err(r)
+        end
+    end
+
+    @testset "div21 error bound" begin
+        output = fn_output["div21"]
+        @test length(args) == length(output)
+        rel_err = 3u^2
+        abs_err = r -> max(abs(rel_err * r), 2e)
+        for (i, ((xhi, xlo), y)) in enumerate(args)
+            zhi, zlo = output[i]
+            z = big(zhi) + big(zlo)
+            r = big(xhi)/big(y) + big(xlo)/big(y)
+            @test abs(z - r) < abs_err(r)
+        end
+    end
+end
+
+@testset "op22(TwoF64, TwoF64)" begin
+    args = args_list.s22
+
+    @testset "add22 error bound" begin
+        output = fn_output["add22"]
+        @test length(args) == length(output)
+        rel_err = 3u^2 + 13u^3
+        abs_err = r -> max(abs(rel_err * r), e)
+        for (i, ((xhi, xlo), (yhi, ylo))) in enumerate(args)
+            zhi, zlo = output[i]
+            z = big(zhi) + big(zlo)
+            r = big(xhi) + big(yhi) + big(xlo) + big(ylo)
+            @test abs(z - r) < abs_err(r)
+        end
+    end
+
+    @testset "mul22 error bound" begin
+        output = fn_output["mul22"]
+        @test length(args) == length(output)
+        rel_err = 5u^2
+        abs_err = r -> max(abs(rel_err * r), 2.5e)
+        for (i, ((xhi, xlo), (yhi, ylo))) in enumerate(args)
+            zhi, zlo = output[i]
+            z = big(zhi) + big(zlo)
+            r = (big(xhi) + big(xlo)) * (big(yhi) + big(ylo))
+            @test abs(z - r) < abs_err(r)
+        end
+    end
+
+    @testset "div22 error bound" begin
+        output = fn_output["div22"]
+        @test length(args) == length(output)
+        rel_err = 15u^2 + 56u^3
+        abs_err = r -> max(abs(rel_err * r), 2.5e)
+        for (i, ((xhi, xlo), (yhi, ylo))) in enumerate(args)
+            zhi, zlo = output[i]
+            z = big(zhi) + big(zlo)
+            r = (big(xhi) + big(xlo)) / (big(yhi) + big(ylo))
+            @test abs(z - r) < abs_err(r)
         end
     end
 end
