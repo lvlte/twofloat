0f5b377628f16d66549d6ca53b8d6c2a8321d091
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-04-09 18:17:29 +0200

    docs: jsdoc description for EFTs and common types/constants

--- src/base/common.ts
+++ src/base/common.ts
@@ -1,7 +1,32 @@
+/**
+ * @file Common types and constants
+ */
 
+/**
+ * Alias for the `number` type.
+ */
 export type f64 = number;
+
+/**
+ * Canonical representation of a floating-point number with extended precision,
+ * ie. whose value is mathematically equal to `hi + lo` and where the non-zero
+ * bits in `hi` and `lo` don't overlap. `hi` contains the most significant bits
+ * and `lo` the least significant.
+ */
 export type TwoF64 = [hi: f64, lo: f64];
 
+/**
+ * Precision of a float64 number (effective number of bits in the significand).
+ */
 export const F64_PRECISION = 53;
-export const F64_TRUNC_NB = Math.ceil(F64_PRECISION/2);
-export const F64_SPLIT_K = 2**F64_TRUNC_NB + 1;
+
+/**
+ * Number of bits that determines the splitting point `F64_SPLITTER` used by the
+ * `split` function.
+ */
+export const F64_SPLIT_NB = 27;           // Math.ceil(F64_PRECISION/2);
+
+/**
+ * Number used to `split` a given number into two halves `hi` and `lo`.
+ */
+export const F64_SPLITTER = 134217729;    // 2**F64_SPLIT_NB + 1;
