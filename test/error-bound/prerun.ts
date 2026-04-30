7dfa62bd40409e97ff5fdc63a9c15d3882422ebc
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-04-30 17:55:20 +0200

    test: move error bound tests in subdir; add test script wrappers

---
+++ test/error-bound/prerun.ts
@@ -0,0 +1,9 @@
+/**
+ * @file Pre-test
+ *
+ * Generates inputs and collect outputs for each functions then export the whole
+ * as JSON so we can import it and do proper tests in Julia using BigFloat.
+ */
+
+import './algorithms';
+
---
+++ test/error-bound/runtest.jl
@@ -0,0 +1,3 @@
+# TwoFloat accuracy tests
+
+include("./algorithms.jl")
