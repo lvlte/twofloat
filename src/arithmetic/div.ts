99ab962a9bb1c6f7013f146d7711169c57cb613b
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-05-27 16:26:46 +0200

    Add functions `div12` and `sub12`

--- src/arithmetic/div.ts
+++ src/arithmetic/div.ts
@@ -5,6 +5,9 @@ import {
   twoInv,
   DWInv,
 } from '../base/algorithms.js';
+import type { f64, TwoF64 } from '../base/common.js';
+import { normalize } from '../base/eft.js';
+import { mul21 } from './mul.js';
 
 export const div11 = twoDiv;
 export const div21 = DWDivFP3;
@@ -12,3 +15,14 @@ export const div22 = DWDivDW2;
 
 export const inv1 = twoInv;
 export const inv2 = DWInv;
+
+/**
+ * Extended-precision computation of `x / (yₕᵢ + yₗₒ)`.
+ *
+ * Relative error bound: `15u² + 56u³` with `u = 2^-53`
+ */
+export function div12(x: f64, [yhi, ylo]: TwoF64): TwoF64 {
+  const hi = x/yhi;
+  const [rhi, rlo] = mul21([yhi, ylo], hi);
+  return normalize(hi, ((x - rhi) - rlo)/yhi);
+}
--- src/arithmetic/sub.ts
+++ src/arithmetic/sub.ts
@@ -1,6 +1,21 @@
-import { twoDiff } from '../base/eft.js';
+/**
+ * @file Arithmetic - Subtraction
+ */
+
+import { normalize, twoDiff } from '../base/eft.js';
 import { DWMinusFP, AccurateDWMinusDW } from '../base/algorithms.js';
+import type { f64, TwoF64 } from '../base/common.js';
 
 export const sub11 = twoDiff;
 export const sub21 = DWMinusFP;
 export const sub22 = AccurateDWMinusDW;
+
+/**
+ * Extended-precision computation of `x - (yₕᵢ + yₗₒ)`.
+ *
+ * Relative error bound: `2u²` with `u = 2^-53`
+ */
+export function sub12(x: f64, [yhi, ylo]: TwoF64): TwoF64 {
+  const [hi, lo] = sub11(x, yhi);
+  return normalize(hi, lo - ylo);
+}
