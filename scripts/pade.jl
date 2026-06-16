02833514b73414b0b6272460c976dbd61492323e
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-06-16 20:01:41 +0200

    Utils for generating Padé approximants from a Taylor series

--- scripts/pade.jl
+++ scripts/pade.jl
@@ -1,5 +1,7 @@
 # Utils for computing Padé approximants
 
+using RowEchelon
+
 """
 Coefficients of the Padé approximant of order [p/q] for e^x, from lowest to
 highest degree.
@@ -20,7 +22,6 @@ function pade_exp(p::T, q::T) where {T<:Integer}
     return (P, Q)
 end
 
-
 """
 Integer coefficients of the Padé approximant of order [n/n] for e^x, from
 highest to lowest degree (absolute values, one vector represent both num & den).
@@ -32,3 +33,62 @@ function pade_exp_int(n::T) where {T<:Integer}
     end
     return A
 end
+
+"""
+Padé `R[m/n](x)` computed from a truncated Taylor series `Tₘ₊ₙ(x)` of degree m+n
+
+  `R[m/n](x) = Pₘ(x)/Qₙ(x) = Tₘ₊ₙ(x) = ∑[k=0..m+n](cₖxᵏ)`
+
+The passed-in function `fn` must return the coefficient `cₖ` of the Taylor
+expansion, given some integer parameter `k::T` in the range `[0..m+n]`.
+
+Return a tuple `(P, Q)` where `P` and `Q` represent respectively the coefficients
+of the polynomials `Pₘ(x)` and `Qₙ(x)`, ordered from lowest to highest degree.
+"""
+function taylor_to_pade(fn::F, m::T, n::T) where {T<:Integer, F<:Function}
+    m ≥ 0 && n ≥ 0 || throw(ArgumentError("Padé order (m and n) must be greater or equal to zero"))
+    C = map(fn, zero(T):m+n)
+
+    # https://www.colorado.edu/amath/sites/default/files/attached-files/pade.pdf
+
+    M = []
+    for k in m+1:m+n, j=k-n
+        cₖ, Qrow... = C[k+1:-1:max(0,j)+1]
+        j < 0 && push!(Qrow, zeros(typeof(cₖ), abs(j))...)
+        push!(M, [Qrow..., -cₖ])
+    end
+
+    Q = [ one(C[1]), (isempty(M) ? [] : rref(stack(M, dims=1))[:,n+1])... ]
+    P = [ sum((C[k-i+1]*Q[i+1] for i in 0:min(k,n))) for k in 0:m ]
+
+    return P, Q
+end
+
+"""
+Transform the given Padé `Pₘ(x)/Qₙ(x)` with rational coefficients into its
+alernate form with integer coefficients.
+"""
+function pade_int(::Type{T}, P::Vector{Rational{U}}, Q::Vector{Rational{U}}) where {T<:Integer, U<:Integer}
+    den = map(r -> r.den, (P..., Q...))
+    k = lcm(den...)
+    P = [ T(k*p) for p in P ]
+    Q = [ T(k*q) for q in Q ]
+    return P, Q
+end
+pade_int(P::Vector{Rational{T}}, Q::Vector{Rational{T}}) where {T<:Integer} = pade_int(T, P, Q)
+
+
+# Taylor series are represented below with functions that return the coefficient
+# `cₙ` (in the expansion of the series) given some integer `n ≥ 0`.
+
+taylor_ln1p(n::Integer) = iszero(n) ? zero(n)//one(n) : (-one(n))^(n+1)//n
+taylor_ln1m(n::Integer) = iszero(n) ? zero(n)//one(n) : -one(n)//n
+taylor_exp(n::Integer) = one(n)//factorial(n)
+
+# taylor_sin(n::Integer) = isodd(n) ? oftype(n,(-1)^((n-1)/2))//factorial(n) : zero(n)//one(n)
+# taylor_cos(n::Integer) = iseven(n) ? oftype(n,(-1)^(n/2))//factorial(n) : zero(n)//one(n)
+
+# skip zero coefficients (n maps to 2n+1 for sin and 2n for cos)
+taylor_sin(n::T) where {T<:Integer} = T(-1)^n//factorial(T(2n+1))
+taylor_cos(n::T) where {T<:Integer} = T(-1)^n//factorial(T(2n))
+
--- scripts/pre.jl
+++ scripts/pre.jl
@@ -2,6 +2,8 @@
 # Output to JSON (override)
 
 using JSON
+using DataStructures
+
 include("./pade.jl")
 
 TwoF64(x::Real) = TwoF64(BigFloat(x))
@@ -21,7 +23,13 @@ function TwoF64Int(x::BigFloat)
     return (hi, lo)
 end
 
-math_const = Dict(
+function F64Int(x::Real)
+    hi, lo = TwoF64Int(x)
+    lo != 0 && throw(InexactError(:F64Int, Float64, x))
+    return hi
+end
+
+math_const = OrderedDict(
     "E" => big(ℯ),
     "LN2" => log(big(2.0)),
     "LN10" => log(big(10.0)),
@@ -32,19 +40,59 @@ math_const = Dict(
     "SQRT2" => sqrt(big(2.0)),
 )
 
+two_const = OrderedDict(key => TwoF64(val) for (key, val) in math_const)
+
+# exp(n)
 nmax = 80
 exp_n = [[n, TwoF64(exp(BigFloat(n)))] for n in -nmax:nmax]
 
-N = UnitRange{Int128}(10, 16)
-exp_pade = Dict(n => pade_exp(n,n)[1] for n in N)
-exp_pade_int = Dict(n => pade_exp_int(n) for n in N)
-
-two_const = Dict(key => TwoF64(val) for (key, val) in math_const)
-pre_exp = Dict(
+# Padé [n/n] exp(x)
+# Integer coefficients can be represented as f64 safe integers for n < 16,
+# higher orders requires TwoF64 but won't bring more accuracy (except if we
+# use more (ie. triple, quad, etc.) precision to evaluate the approximant.
+N = UnitRange{Int128}(10, 15)
+exp_pade = OrderedDict(n => pade_exp(n,n)[1] for n in N)
+exp_pade_int = OrderedDict(n => pade_exp_int(n) for n in N)
+pre_exp = OrderedDict(
     "exp_n" => [exp_n; [["nmax", nmax]]],
-    "exp_pade" => Dict(n => TwoF64.(exp_pade[n]) for n in N),
-    "exp_pade_int" => Dict(n => TwoF64Int.(exp_pade_int[n]) for n in N)
+    "exp_pade" => OrderedDict(n => TwoF64.(exp_pade[n]) for n in N),
+    "exp_pade_int" => OrderedDict(n => F64Int.(exp_pade_int[n]) for n in N)
 )
 
+# Padé [n/n] ln(x + 1)
+N = UnitRange{Int128}(12, 22)
+ln1p_pade = OrderedDict(n => taylor_to_pade(taylor_ln1p, n, n) for n in N)
+ln1p_pade_int = OrderedDict(n => reverse.(pade_int(ln1p_pade[n]...)) for n in N)
+pre_log = OrderedDict(
+    "ln1p_pade" => OrderedDict(n => [TwoF64.(ln1p_pade[n][1]), TwoF64.(ln1p_pade[n][2])] for n in N),
+    "ln1p_pade_int" => OrderedDict(n => [TwoF64.(ln1p_pade_int[n][1]), TwoF64.(ln1p_pade_int[n][2])] for n in N)
+)
+
+# Padé [n/n] sin(x)
+#   use m,n pairs instead of n to skip the trivial zeros (NB. given the vector
+#   pair (P, Q) representing the Padé approximant R[n/n](x) = Pₙ(x)/Qₙ(x), P[k]
+#   holds the coefficient of degree 2k+1, and Q[k] the coefficient of degree 2k)
+N = UnitRange{BigInt}(20, 30)
+MN = map(n -> (div(n-1, 2), div(n, 2)), N)
+sin_pade = OrderedDict(m+n+1 => taylor_to_pade(taylor_sin, m, n) for (m,n) in MN)
+sin_pade_TwoF64 = OrderedDict(n => [TwoF64.(sin_pade[n][1]), TwoF64.(sin_pade[n][2])] for n in N)
+
+# Padé [2n/2n] cos(x)
+# Same logic here except both P[k] and Q[k] map to x^2k (like in tne Taylor
+# expansion, every odd power of x is 0 zero whatever the Padé order)
+N = UnitRange{BigInt}(10, 15)
+cos_pade = OrderedDict(2n => taylor_to_pade(taylor_cos, n, n) for n in N)
+cos_pade_TwoF64 = OrderedDict(2n => [TwoF64.(cos_pade[2n][1]), TwoF64.(cos_pade[2n][2])] for n in N)
+
+pre_trig = OrderedDict(
+    "sin_pade" => sin_pade_TwoF64,
+    "cos_pade" => cos_pade_TwoF64,
+)
+
+
+### Output
+
 JSON.json("$(@__DIR__)/constants.json", two_const; pretty=true)
-JSON.json("$(@__DIR__)/exp.json", pre_exp)
+JSON.json("$(@__DIR__)/exp.json", pre_exp; pretty=true)
+JSON.json("$(@__DIR__)/log.json", pre_log; pretty=true)
+JSON.json("$(@__DIR__)/trig.json", pre_trig; pretty=true)
