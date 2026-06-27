12282291a2dbcd8a76b35a972a1190c5c0854ecb
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-06-27 15:13:14 +0200

    Test: (math) use `processArgsFn` instead of wrapping functions

--- test/error-bound/math.ts
+++ test/error-bound/math.ts
@@ -3,81 +3,19 @@
  */
 
 import {
-  F64_SPLITTER,
-  normalize,
-  abs2,
-  square1,
-  square2,
-  cube1,
-  cube2,
-  _linpow as __linpow,
-  _logpow as __logpow,
-  _logpowltr as __logpowltr,
-  _linpow2 as __linpow2,
-  _logpow2 as __logpow2,
-  sqrt1 as _sqrt1,
-  sqrt2 as _sqrt2,
-  add11,
-  exp1,
-  exp2,
-  ln1 as _ln1,
-  ln2 as _ln2,
-  log2_1 as _log2_1,
-  log2_2 as _log2_2,
-  log10_1 as _log10_1,
-  log10_2 as _log10_2,
-  pow1int,
-  pow2int,
-  pow11 as _pow11,
-  pow12 as _pow12,
-  pow21 as _pow21,
-  pow22 as _pow22,
-  rem2pi_1,
-  rem2pi_2,
-  rempi_1,
-  rempi_2,
-  sin1,
-  sin2,
-  cos1,
-  cos2,
-  tan1,
-  tan2,
-  cot1,
-  cot2,
-  sec1,
-  csc1,
-  sec2,
-  csc2,
+  F64_SPLITTER, normalize, abs2, add11,
+  square1, square2, cube1, cube2,
+  exp1, exp2, pow1int, pow2int, pow11, pow12, pow21, pow22,
+  _linpow, _logpow, _logpowltr, _linpow2, _logpow2,
+  sqrt1, sqrt2,
+  ln1, ln2, log2_1, log2_2, log10_1, log10_2,
+  rem2pi_1, rem2pi_2, rempi_1, rempi_2,
+  sin1, sin2, cos1, cos2, tan1, tan2, cot1, cot2, sec1, csc1, sec2, csc2,
 } from '../../src/index';
 
+import { FnSig, UnionToIntersection, Expand, randomFn } from '../utils';
 import { exponent } from '@lvlte/ulp';
 import fs from 'node:fs';
-
-import {
-  FnSig,
-  UnionToIntersection,
-  Expand,
-  randomFn,
-} from '../utils';
-
-// Wrap some functions so that they are tested with inputs that fit their domain
-const sqrt1: typeof _sqrt1 = x => _sqrt1(Math.abs(x));
-const sqrt2: typeof _sqrt2 = x => _sqrt2(abs2(x));
-const ln1: typeof _ln1 = x => _ln1(Math.abs(x));
-const ln2: typeof _ln2 = x => _ln2(abs2(x));
-const log2_1: typeof _log2_1 = x => _log2_1(Math.abs(x));
-const log2_2: typeof _log2_2 = x => _log2_2(abs2(x));
-const log10_1: typeof _log10_1 = x => _log10_1(Math.abs(x));
-const log10_2: typeof _log10_2 = x => _log10_2(abs2(x));
-const _linpow: typeof __linpow = (x, n) => __linpow(x, Math.abs(n));
-const _logpow: typeof __logpow = (x, n) => __logpow(x, Math.abs(n));
-const _logpowltr: typeof __logpowltr = (x, n) => __logpowltr(x, Math.abs(n));
-const _linpow2: typeof __linpow2 = (x, n) => __linpow2(x, Math.abs(n));
-const _logpow2: typeof __logpow2 = (x, n) => __logpow2(x, Math.abs(n));
-const pow11: typeof _pow11 = (x, p) => _pow11(Math.abs(x), p);
-const pow12: typeof _pow12 = (x, p) => _pow12(Math.abs(x), p);
-const pow21: typeof _pow21 = (x, p) => _pow21(abs2(x), p);
-const pow22: typeof _pow22 = (x, p) => _pow22(abs2(x), p);
 
 // Functions to test grouped by signature
 const fnBySig = {
@@ -103,6 +41,38 @@ type FnName = keyof TestedFunctions;
 
 type ArgsListBySig = { [K in keyof FnBySig]: Parameters<FnSig[K]>[] };
 type FnOutputList = { [K in FnName]: ReturnType<TestedFunctions[K]>[] }
+type FnArgs = { [K in FnName]: Parameters<TestedFunctions[K]> };
+
+// Return the appropriate processArgs callback for the given function. The role
+// of the callback is to make the function parameters fit its domain if needed.
+function processArgsFn(fnName: FnName): Function {
+  switch (fnName) {
+    case 'sqrt1':
+    case 'ln1':
+    case 'log2_1':
+    case 'log10_1':
+    case 'pow11':
+    case 'pow12':
+      return (...args: FnArgs[typeof fnName]) => (args[0] = Math.abs(args[0]), args);
+
+    case 'sqrt2':
+    case 'ln2':
+    case 'log2_2':
+    case 'log10_2':
+    case 'pow21':
+    case 'pow22':
+      return (...args: FnArgs[typeof fnName]) => (args[0] = abs2(args[0]), args);
+
+    case '_linpow':
+    case '_logpow':
+    case '_logpowltr':
+    case '_linpow2':
+    case '_logpow2':
+      return (...args: FnArgs[typeof fnName]) => (args[1] = Math.abs(args[1]), args);
+  }
+
+  return (...args: FnArgs[typeof fnName]) => args;
+}
 
 // Pseudo-random number generator
 const SEED = Math.sqrt(2);
@@ -185,10 +155,12 @@ for (const sid in fnBySig) {
   for (const fnName in fnGroup) {
     fnOutput[fnName as FnName] = [];
     const fn = (fnGroup as TestedFunctions)[fnName as FnName];
-    const fnOut = fnOutput[fnName as FnName]!;
+    const processArgs = processArgsFn(fnName as FnName);
+    const fnOut = fnOutput[fnName as FnName];
     for (const args of argsGroup) {
+      const _args = processArgs(...args) as typeof args;
       // @ts-ignore (TS doesn't understand correlated unions)
-      const result = fn(...args);
+      const result = fn(..._args);
       fnOut.push(result);
     }
   }
