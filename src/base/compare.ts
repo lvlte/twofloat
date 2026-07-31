b6325e4d635cb03e336f971cf2a2f4d4d74bdd50
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-07-31 22:21:08 +0200

    Generic comparison functions (wrapper dispatch to typed functions)

--- src/base/compare.ts
+++ src/base/compare.ts
@@ -24,7 +24,15 @@ export function eq22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is less than `y`.
+ * `x < y` — Return a boolean indicating whether `x` is less than `y`.
+ */
+export function lt12(x: f64, y: TwoF64): boolean;
+export function lt12(x: f64, [yhi, ylo]: TwoF64): boolean {
+  return x < yhi || (x === yhi && ylo > 0);
+}
+
+/**
+ * `x < y` — Return a boolean indicating whether `x` is less than `y`.
  */
 export function lt21(x: TwoF64, y: f64): boolean;
 export function lt21([xhi, xlo]: TwoF64, y: f64): boolean {
@@ -32,7 +40,7 @@ export function lt21([xhi, xlo]: TwoF64, y: f64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is less than `y`.
+ * `x < y` — Return a boolean indicating whether `x` is less than `y`.
  */
 export function lt22(x: TwoF64, y: TwoF64): boolean;
 export function lt22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
@@ -40,7 +48,15 @@ export function lt22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is less than or equal to `y`.
+ * `x ≤ y` — Return a boolean indicating whether `x` is less than or equal to `y`.
+ */
+export function le12(x: f64, y: TwoF64): boolean;
+export function le12(x: f64, [yhi, ylo]: TwoF64): boolean {
+  return x < yhi || (x === yhi && ylo >= 0);
+}
+
+/**
+ * `x ≤ y` — Return a boolean indicating whether `x` is less than or equal to `y`.
  */
 export function le21(x: TwoF64, y: f64): boolean;
 export function le21([xhi, xlo]: TwoF64, y: f64): boolean {
@@ -48,7 +64,7 @@ export function le21([xhi, xlo]: TwoF64, y: f64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is less than or equal to `y`.
+ * `x ≤ y` — Return a boolean indicating whether `x` is less than or equal to `y`.
  */
 export function le22(x: TwoF64, y: TwoF64): boolean
 export function le22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
@@ -56,7 +72,15 @@ export function le22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is greater than `y`.
+ * `x > y` — Return a boolean indicating whether `x` is greater than `y`.
+ */
+export function gt12(x: f64, y: TwoF64): boolean;
+export function gt12(x: f64, [yhi, ylo]: TwoF64): boolean {
+  return x > yhi || (x === yhi && ylo < 0);
+}
+
+/**
+ * `x > y` — Return a boolean indicating whether `x` is greater than `y`.
  */
 export function gt21(x: TwoF64, y: f64): boolean;
 export function gt21([xhi, xlo]: TwoF64, y: f64): boolean {
@@ -64,7 +88,7 @@ export function gt21([xhi, xlo]: TwoF64, y: f64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is greater than `y`.
+ * `x > y` — Return a boolean indicating whether `x` is greater than `y`.
  */
 export function gt22(x: TwoF64, y: TwoF64): boolean;
 export function gt22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
@@ -72,7 +96,15 @@ export function gt22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is greater than or equal to `y`.
+ * `x ≥ y` — Return a boolean indicating whether `x` is greater than or equal to `y`.
+ */
+export function ge12(x: f64, y: TwoF64): boolean;
+export function ge12(x: f64, [yhi, ylo]: TwoF64): boolean {
+  return x > yhi || (x === yhi && ylo <= 0);
+}
+
+/**
+ * `x ≥ y` — Return a boolean indicating whether `x` is greater than or equal to `y`.
  */
 export function ge21(x: TwoF64, y: f64): boolean;
 export function ge21([xhi, xlo]: TwoF64, y: f64): boolean {
@@ -80,7 +112,7 @@ export function ge21([xhi, xlo]: TwoF64, y: f64): boolean {
 }
 
 /**
- * Return a boolean indicating whether `x` is greater than or equal to `y`.
+ * `x ≥ y` — Return a boolean indicating whether `x` is greater than or equal to `y`.
  */
 export function ge22(x: TwoF64, y: TwoF64): boolean;
 export function ge22([xhi, xlo]: TwoF64, [yhi, ylo]: TwoF64): boolean {
@@ -147,4 +179,49 @@ export function isSafeTwoInteger([xhi, xlo]: TwoF64): boolean {
 export function isNaN2(x: TwoF64): boolean;
 export function isNaN2([xhi, xlo]: TwoF64): boolean {
   return Number.isNaN(xhi + xlo);
+}
+
+/**
+ * Return a boolean indicating whether `x` is equal to `y`.
+ */
+export function eq(x: f64 | TwoF64, y: f64 | TwoF64): boolean {
+  return typeof x === 'number'
+    ? typeof y === 'number' ? x === y : eq21(y, x)
+    : typeof y === 'number' ? eq21(x, y) : eq22(x, y);
+}
+
+/**
+ * `x < y` — Return a boolean indicating whether `x` is less than `y`.
+ */
+export function lt(x: f64 | TwoF64, y: f64 | TwoF64): boolean {
+  return typeof x === 'number'
+    ? typeof y === 'number' ? x < y : lt12(x, y)
+    : typeof y === 'number' ? lt21(x, y) : lt22(x, y)
+}
+
+/**
+ * `x ≤ y` — Return a boolean indicating whether `x` is less than or equal to `y`.
+ */
+export function le(x: f64 | TwoF64, y: f64 | TwoF64): boolean {
+  return typeof x === 'number'
+    ? typeof y === 'number' ? x <= y : le12(x, y)
+    : typeof y === 'number' ? le21(x, y) : le22(x, y)
+}
+
+/**
+ * `x > y` — Return a boolean indicating whether `x` is greater than `y`.
+ */
+export function gt(x: f64 | TwoF64, y: f64 | TwoF64): boolean {
+  return typeof x === 'number'
+    ? typeof y === 'number' ? x > y : gt12(x, y)
+    : typeof y === 'number' ? gt21(x, y) : lt22(x, y)
+}
+
+/**
+ * `x ≥ y` — Return a boolean indicating whether `x` is greater than or equal to `y`.
+ */
+export function ge(x: f64 | TwoF64, y: f64 | TwoF64): boolean {
+  return typeof x === 'number'
+    ? typeof y === 'number' ? x >= y : ge12(x, y)
+    : typeof y === 'number' ? ge21(x, y) : ge22(x, y)
 }
