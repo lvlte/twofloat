0fea1582f8b158d3b16370e571c5eb396c32f88f
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-08-07 19:35:49 +0200

    Add rounding functions `trunc`, `floor`, `ceil`, `round`

--- src/math/basic.ts
+++ src/math/basic.ts
@@ -5,6 +5,7 @@
  */
 
 import { NaN2, type TwoF64 } from '../base/common.js';
+import { normalize } from '../base/eft.js';
 
 /**
  * Return the absolute value of `x`.
@@ -39,4 +40,81 @@ export function sign(x: TwoF64): TwoF64;
 export function sign([xhi, xlo]: TwoF64): TwoF64 {
   const sign = Math.sign(xhi + xlo);
   return Number.isFinite(sign) ? [sign, 0] : NaN2;
+}
+
+/**
+ * Return the integral part of `x`, removing any fractional digits.
+ *
+ * @param x A `TwoF64` number
+ * @returns The {@link TwoF64|`TwoF64`} representation of the integer part of `x`
+ */
+export function trunc(x: TwoF64): TwoF64;
+export function trunc([xhi, xlo]: TwoF64): TwoF64 {
+  if (!Number.isFinite(xhi)) {
+    return Number.isNaN(xhi) || Math.abs(xhi) !== Infinity ? NaN2 : [xhi, 0];
+  }
+
+  if (Number.isInteger(xhi)) {
+    return Number.isInteger(xlo) ? [xhi, xlo] : normalize(xhi, Math.trunc(xlo));
+  }
+
+  return [Math.trunc(xhi), 0];
+}
+
+/**
+ * Return `⌊x⌋`, the greatest integer less than or equal to `x`.
+ *
+ * @param x A `TwoF64` number
+ * @returns The {@link TwoF64|`TwoF64`} representation of `⌊x⌋`
+ */
+export function floor(x: TwoF64): TwoF64;
+export function floor([xhi, xlo]: TwoF64): TwoF64 {
+  if (!Number.isFinite(xhi)) {
+    return Number.isNaN(xhi) || Math.abs(xhi) !== Infinity ? NaN2 : [xhi, 0];
+  }
+
+  if (Number.isInteger(xhi)) {
+    return Number.isInteger(xlo) ? [xhi, xlo] : normalize(xhi, Math.floor(xlo));
+  }
+
+  return [Math.floor(xhi), 0];
+}
+
+/**
+ * Return `⌈x⌉`, the least integer greater than or equal to `x`.
+ *
+ * @param x A `TwoF64` number
+ * @returns The {@link TwoF64|`TwoF64`} representation of `⌈x⌉`
+ */
+export function ceil(x: TwoF64): TwoF64;
+export function ceil([xhi, xlo]: TwoF64): TwoF64 {
+  if (!Number.isFinite(xhi)) {
+    return Number.isNaN(xhi) || Math.abs(xhi) !== Infinity ? NaN2 : [xhi, 0];
+  }
+
+  if (Number.isInteger(xhi)) {
+    return Number.isInteger(xlo) ? [xhi, xlo] : normalize(xhi, Math.ceil(xlo));
+  }
+
+  return [Math.ceil(xhi), 0];
+}
+
+/**
+ * Return the nearest integer to `x`, with ties (fractional values of `0.5`)
+ * being rounded to the nearest **even** integer.
+ *
+ * @param x A `TwoF64` number
+ * @returns The {@link TwoF64|`TwoF64`} representation of `x`
+ */
+export function round(x: TwoF64): TwoF64;
+export function round([xhi, xlo]: TwoF64): TwoF64 {
+  if (!Number.isFinite(xhi)) {
+    return Number.isNaN(xhi) || Math.abs(xhi) !== Infinity ? NaN2 : [xhi, 0];
+  }
+
+  if (Number.isInteger(xhi)) {
+    return Number.isInteger(xlo) ? [xhi, xlo] : normalize(xhi, Math.round(xlo));
+  }
+
+  return [Math.round(xhi), 0];
 }
